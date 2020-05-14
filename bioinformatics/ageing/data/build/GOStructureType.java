97ad2519fdf390ac4a5841b0995b24b95760d819
Pablo da Silva <pablo.dasilva@dell.com>
2020-05-14 08:45:07 -0300

    files appl int

--- bioinformatics/ageing/data/build/GOStructureType.java
+++
@@ -1,8 +0,0 @@
-package bioinformatics.ageing.data.build;
-
-/**
- * Created by pablonsilva on 1/15/16.
- */
-public enum GOStructureType {
-    BP, CC, MF, BP_CC, BP_MF, CC_MF, BP_CC_MF, Unique
-}
--- bioinformatics/ageing/data/build/GOType.java
+++
@@ -1,5 +0,0 @@
-package bioinformatics.ageing.data.build;
-
-public enum GOType {
-    biological_process, molecular_function, cellular_component
-}
